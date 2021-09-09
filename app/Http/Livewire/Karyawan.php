bb990897c71c1a8fffbfc23c8c3d0ad6023a5c9c
musyahya <mananabdul37654@gmail.com>
2021-09-09 11:03:43 +0700

    tambah karyawan

--- app/Http/Livewire/Karyawan.php
+++ app/Http/Livewire/Karyawan.php
@@ -3,10 +3,58 @@
 namespace App\Http\Livewire;
 
 use App\Models\Karyawan as ModelsKaryawan;
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Validation\Rules\Password;
 
 class Karyawan extends Component
 {
+    public $tambah;
+    public $nama, $email, $password, $password_confirmation, $alamat, $hp;
+
+    protected function rules()
+    {
+        return [
+            'nama' => 'required',
+            'email' => ['required', 'email', 'unique:App\Models\User,email'],
+            'password' => ['required', Password::min(8), 'confirmed'],
+            'hp' => ['required', 'numeric', 'digits:12'],
+            'alamat' => ['required'],
+        ];
+    }
+
+    public function show_tambah()
+    {
+        $this->tambah = true;
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        $user = User::create([
+            'name' => $this->nama, 
+            'email' => $this->email, 
+            'password' => bcrypt($this->password),
+            'role_id' => 2
+        ]);
+
+        ModelsKaryawan::create([
+            'user_id' => $user->id,
+            'hp' => $this->hp,
+            'alamat' => $this->alamat
+        ]);
+
+        session()->flash('sukses', 'Data berhasil ditambahkan.');
+        $this->format();
+    }
+
+    public function format()
+    {
+        unset($this->nama, $this->email, $this->password, $this->password_confirmation, $this->hp, $this->alamat);
+        $this->tambah = false;
+    }
+
     public function render()
     {
         $karyawan = ModelsKaryawan::all();
--- resources/views/livewire/karyawan.blade.php
+++ resources/views/livewire/karyawan.blade.php
@@ -1,34 +1,45 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-          <div class="list-group">
-            <a href="/dashboard" class="list-group-item list-group-item-action">Dashboard</a>
-            <a href="/karyawan" class="list-group-item list-group-item-action">Karyawan</a>
-        </div>
+            <div class="list-group">
+                <a href="/dashboard" class="list-group-item list-group-item-action">Dashboard</a>
+                <a href="/karyawan" class="list-group-item list-group-item-action">Karyawan</a>
+            </div>
         </div>
         <div class="col-md-9">
             <h2>Halaman Karyawan</h2>
 
+            @include('layouts/karyawan/tambah')
+            @if (session()->has('sukses'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('sukses') }}
+                </div>
+            @endif
+
+            <button wire:click="show_tambah" type="button" class="btn btn-primary btn-sm mb-3">
+                Tambah Karyawan
+            </button>
+
             <table class="table table-striped">
                 <thead>
-                  <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Hp</th>
-                    <th scope="col">Alamat</th>
-                  </tr>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Hp</th>
+                        <th scope="col">Alamat</th>
+                    </tr>
                 </thead>
                 <tbody>
-                  @foreach ($karyawan as $item)
-                    <tr>
-                        <th scope="row">{{$loop->iteration}}</th>
-                        <td>{{$item->user->name}}</td>
-                        <td>{{$item->hp}}</td>
-                        <td>{{$item->alamat}}</td>
-                    </tr>
-                  @endforeach
+                    @foreach ($karyawan as $item)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $item->user->name }}</td>
+                            <td>{{ $item->hp }}</td>
+                            <td>{{ $item->alamat }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
-              </table>
+            </table>
         </div>
     </div>
 </div>
