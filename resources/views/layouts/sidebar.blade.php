37d3c6767a5d5c02f2c47d7ce6b8c43d527a6bd0
musyahya <mananabdul37654@gmail.com>
2021-09-11 17:12:08 +0700

    gate admin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,8 @@
 <div class="list-group">
     <a href="/dashboard" class="list-group-item list-group-item-action">Dashboard</a>
+    @can('admin')
     <a href="/karyawan" class="list-group-item list-group-item-action">Karyawan</a>
+    @endcan
     <a href="/layanan" class="list-group-item list-group-item-action">Layanan</a>
     <a href="/transaksi" class="list-group-item list-group-item-action">Transaksi</a>
     <a href="/progres" class="list-group-item list-group-item-action">Progres</a>
