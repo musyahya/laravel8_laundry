8d0473427501891710811a8d843d52f197ec322a
musyahya <mananabdul37654@gmail.com>
2021-09-11 17:03:35 +0700

    admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()->role_id != '1') {
+            return redirect()->back();
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', \App\Http\Livewire\Dashboard::class);
-    Route::get('/karyawan', \App\Http\Livewire\Karyawan::class);
     Route::get('/layanan', \App\Http\Livewire\Layanan::class);
     Route::get('/transaksi', \App\Http\Livewire\Transaksi::class);
     Route::get('/progres', \App\Http\Livewire\Progres::class);
     Route::get('/pembayaran', \App\Http\Livewire\Pembayaran::class);
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/karyawan', \App\Http\Livewire\Karyawan::class);
 });
